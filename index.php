68b20376eefb229b9a3e297727e7c57ba7585a79
rodrigor.oliveira <rodrigor.oliveira@gmail.com>
2015-07-31 10:52:30 -0300

    Primeiro comite

--- index.php
+++ index.php
@@ -1,147 +1,292 @@
-<!DOCTYPE HTML>
-<html>
-<?php include('head.php'); ?>
-		<!-- Jquery para SlideShow -->
-		<script type="text/javascript">
-			jQuery(function($){
-				$.supersized({
-						// Functionality
-						slideshow      : 1, // Slideshow on/off
-						autoplay        : 1, // Slideshow starts playing automatically
-						start_slide     : 1, // Start slide (0 is random)
-						stop_loop      : 0, // Pauses slideshow on last slide
-						random         : 0, // Randomize slide order (Ignores start slide)
-						slide_interval : 3000,// Length between transitions
-						transition       : 1, // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-						transition_speed: 1000, // Speed of transition
-						new_window      : 1, // Image links open in new window/tab
-						pause_hover     : 0, // Pause slideshow on hover
-						keyboard_nav    : 1, // Keyboard navigation on/off
-						performance     : 1, // 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
-						image_protect   : 1, // Disables image dragging and right click with Javascript
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2015, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
 
-						// Size & Position
-						min_width       : 0, // Min width allowed (in pixels)
-						min_height      : 0, // Min height allowed (in pixels)
-						vertical_center : 1, // Vertically center background
-						horizontal_center:1, // Horizontally center background
-						fit_always      : 0, // Image will never exceed browser width or height (Ignores min. dimensions)
-						fit_portrait    : 1, // Portrait images will not exceed browser height
-						fit_landscape   : 0, // Landscape images will not exceed browser width
+/*
+ *---------------------------------------------------------------
+ * APPLICATION ENVIRONMENT
+ *---------------------------------------------------------------
+ *
+ * You can load different configurations depending on your
+ * current environment. Setting the environment also influences
+ * things like logging and error reporting.
+ *
+ * This can be set to anything, but default usage is:
+ *
+ *     development
+ *     testing
+ *     production
+ *
+ * NOTE: If you change these, also change the error_reporting() code below
+ */
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
-						// Components
-						slide_links     : 'blank',  // Individual links for each slide (Options: false, 'num', 'name', 'blank')
-						thumb_links     : 1, // Individual thumb links for each slide
-						thumbnail_navigation : 0, // Thumbnail navigation
-						slides          :[ // Slideshow Images
-										{image : 'images/enroladinhos.jpg', title : 'Enroladinhos'},
-										{image : 'images/miniBaguetes.jpg', title : 'Baguetes'},
-										{image : 'images/cupcakes.jpg', title : 'cupcakes'},
-										{image : 'images/barcaBrigadeiro.jpg', title : 'brigadeiro'},
-									],
+/*
+ *---------------------------------------------------------------
+ * ERROR REPORTING
+ *---------------------------------------------------------------
+ *
+ * Different environments will require different levels of error reporting.
+ * By default development will show errors but testing and live will hide them.
+ */
+switch (ENVIRONMENT)
+{
+	case 'development':
+		error_reporting(-1);
+		ini_set('display_errors', 1);
+	break;
 
-						// Theme Options
-						progress_bar    : 1, // Timer for each slide
-						mouse_scrub     : 0
+	case 'testing':
+	case 'production':
+		ini_set('display_errors', 0);
+		if (version_compare(PHP_VERSION, '5.3', '>='))
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+		}
+		else
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+		}
+	break;
 
-				});
-			});
-		</script>
+	default:
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'The application environment is not set correctly.';
+		exit(1); // EXIT_ERROR
+}
 
-<body class="homepage">
+/*
+ *---------------------------------------------------------------
+ * SYSTEM FOLDER NAME
+ *---------------------------------------------------------------
+ *
+ * This variable must contain the name of your "system" folder.
+ * Include the path if the folder is not in the same directory
+ * as this file.
+ */
+	$system_path = 'system';
 
-		<!-- Header -->
-		<div id="header">
-			<!-- Inner -->
-			<div class="inner">
-			<?php include('logo.php'); ?>
-					<a href="#banner" class="button circled scrolly logo-btn-click">Delícias!</a>
-			</div>
-			<!-- Nav -->
-			<?php include('menu.php'); ?>
+/*
+ *---------------------------------------------------------------
+ * APPLICATION FOLDER NAME
+ *---------------------------------------------------------------
+ *
+ * If you want this front controller to use a different "application"
+ * folder than the default one you can set its name here. The folder
+ * can also be renamed or relocated anywhere on your server. If
+ * you do, use a full server path. For more info please see the user guide:
+ * http://codeigniter.com/user_guide/general/managing_apps.html
+ *
+ * NO TRAILING SLASH!
+ */
+	$application_folder = 'application';
 
-		</div>
+/*
+ *---------------------------------------------------------------
+ * VIEW FOLDER NAME
+ *---------------------------------------------------------------
+ *
+ * If you want to move the view folder out of the application
+ * folder set the path to the folder here. The folder can be renamed
+ * and relocated anywhere on your server. If blank, it will default
+ * to the standard location inside your application folder. If you
+ * do move this, use the full server path to this folder.
+ *
+ * NO TRAILING SLASH!
+ */
+	$view_folder = '';
 
-		<!-- Banner -->
-		<section id="banner">w
-			<header>
-				<h2><strong>HUUMMM!!!</strong> Ficou com água na boca ?!</h2>
-				<p>
-					Vários produtos com sabores inesquecíveis.
-				</p>
-			</header>
-		</section>
 
-		<!-- Carousel -->
-		<section class="carousel" id="carousel">
-				<div class="reel">
-						<article>
-								<a href="salgados-fritos.php" class="image featured"><img src="images/coxinha.jpg" alt="" /></a>
-								<header>
-									<h3><a href="#">Coxinha de Frango</a></h3>
-								</header>
-								<div class="price">
-									R$ 37 o Cento
-								</div>
-								<p>Crocantes por fora e sequinhos por dentro.</p>
-						</article>
+/*
+ * --------------------------------------------------------------------
+ * DEFAULT CONTROLLER
+ * --------------------------------------------------------------------
+ *
+ * Normally you will set your default controller in the routes.php file.
+ * You can, however, force a custom routing by hard-coding a
+ * specific controller class/function here. For most applications, you
+ * WILL NOT set your routing here, but it's an option for those
+ * special instances where you might want to override the standard
+ * routing in a specific front controller that shares a common CI installation.
+ *
+ * IMPORTANT: If you set the routing here, NO OTHER controller will be
+ * callable. In essence, this preference limits your application to ONE
+ * specific controller. Leave the function name blank if you need
+ * to call functions dynamically via the URI.
+ *
+ * Un-comment the $routing array below to use this feature
+ */
+	// The directory name, relative to the "controllers" folder.  Leave blank
+	// if your controller is not in a sub-folder within the "controllers" folder
+	// $routing['directory'] = '';
 
-						<article>
-								<a href="salgados-assados.php" class="image featured"><img src="images/empada.jpg" alt="" /></a>
-								<header>
-									<h3><a href="#">Empadas</a></h3>
-								</header>
-								<div class="price">
-									R$ 2 a Unid.
-								</div>
-								<p>Nos sabores, Frango, Palmito, Carne seca.</p>
-						</article>
+	// The controller class file name.  Example:  mycontroller
+	// $routing['controller'] = '';
 
-						<article>
-								<a href="baguetes.php" class="image featured"><img src="images/baguete.jpg" alt="" /></a>
-								<header>
-									<h3><a href="#">Baguetes</a></h3>
-								</header>
-								<div class="price">
-										R$ 22 a Unid.
-								</div>
-								<p>Nos sabores de Atum e Salame. Entre outros.<p>
-						</article>
+	// The controller function you wish to be called.
+	// $routing['function']	= '';
 
-						<article>
-								<a href="mini-pizzas.php" class="image featured"><img src="images/miniPizzas.jpg" alt="" /></a>
-								<header>
-									<h3><a href="#">Mine Pizzas</a></h3>
-								</header>
-								<div class="price">
-									R$ 50 o Cento
-								</div>
-								<p>Mini pizzas com sabores variádos. Confira</p>
-						</article>
 
-						<article>
-								<a href="pasteis.php" class="image featured"><img src="images/pastelPodre.jpg" alt="" /></a>
-								<header>
-									<h3><a href="#">Pastéis</a></h3>
-								</header>
-								<div class="price">
-									R$ 37 o Cento
-								</div>
-								<p>Pastéis de vários sabores de dar água na boca</p>
-						</article>
+/*
+ * -------------------------------------------------------------------
+ *  CUSTOM CONFIG VALUES
+ * -------------------------------------------------------------------
+ *
+ * The $assign_to_config array below will be passed dynamically to the
+ * config class when initialized. This allows you to set custom config
+ * items or override any default config values found in the config.php file.
+ * This can be handy as it permits you to share one application between
+ * multiple front controller files, with each file containing different
+ * config values.
+ *
+ * Un-comment the $assign_to_config array below to use this feature
+ */
+	// $assign_to_config['name_of_config_item'] = 'value of config item';
 
-						<article>
-								<a href="doces.php" class="image featured"><img src="images/brigadeiros.jpg" alt="" /></a>
-								<header>
-										<h3><a href="#">Brigadeiros</a></h3>
-								</header>
-								<div class="price">
-									R$ 40 o Cento
-								</div>
-								<p>Brigadeiros de dar água na boca. Confira</p>
-						</article>
-				</div>
-		</section>
-		<?php include('footer.php'); ?>
-</body>
-</html>
+
+
+// --------------------------------------------------------------------
+// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
+// --------------------------------------------------------------------
+
+/*
+ * ---------------------------------------------------------------
+ *  Resolve the system path for increased reliability
+ * ---------------------------------------------------------------
+ */
+
+	// Set the current directory correctly for CLI requests
+	if (defined('STDIN'))
+	{
+		chdir(dirname(__FILE__));
+	}
+
+	if (($_temp = realpath($system_path)) !== FALSE)
+	{
+		$system_path = $_temp.'/';
+	}
+	else
+	{
+		// Ensure there's a trailing slash
+		$system_path = rtrim($system_path, '/').'/';
+	}
+
+	// Is the system path correct?
+	if ( ! is_dir($system_path))
+	{
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
+		exit(3); // EXIT_CONFIG
+	}
+
+/*
+ * -------------------------------------------------------------------
+ *  Now that we know the path, set the main path constants
+ * -------------------------------------------------------------------
+ */
+	// The name of THIS file
+	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+
+	// Path to the system folder
+	define('BASEPATH', str_replace('\\', '/', $system_path));
+
+	// Path to the front controller (this file)
+	define('FCPATH', dirname(__FILE__).'/');
+
+	// Name of the "system folder"
+	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
+
+	// The path to the "application" folder
+	if (is_dir($application_folder))
+	{
+		if (($_temp = realpath($application_folder)) !== FALSE)
+		{
+			$application_folder = $_temp;
+		}
+
+		define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
+	}
+	else
+	{
+		if ( ! is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
+		{
+			header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+			echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+			exit(3); // EXIT_CONFIG
+		}
+
+		define('APPPATH', BASEPATH.$application_folder.DIRECTORY_SEPARATOR);
+	}
+
+	// The path to the "views" folder
+	if ( ! is_dir($view_folder))
+	{
+		if ( ! empty($view_folder) && is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
+		{
+			$view_folder = APPPATH.$view_folder;
+		}
+		elseif ( ! is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
+		{
+			header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+			echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+			exit(3); // EXIT_CONFIG
+		}
+		else
+		{
+			$view_folder = APPPATH.'views';
+		}
+	}
+
+	if (($_temp = realpath($view_folder)) !== FALSE)
+	{
+		$view_folder = $_temp.DIRECTORY_SEPARATOR;
+	}
+	else
+	{
+		$view_folder = rtrim($view_folder, '/\\').DIRECTORY_SEPARATOR;
+	}
+
+	define('VIEWPATH', $view_folder);
+
+/*
+ * --------------------------------------------------------------------
+ * LOAD THE BOOTSTRAP FILE
+ * --------------------------------------------------------------------
+ *
+ * And away we go...
+ */
+require_once BASEPATH.'core/CodeIgniter.php';
