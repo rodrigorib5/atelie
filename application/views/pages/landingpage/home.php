76ad0a70539049eb8560d07707632433505ea9fb
rodrigo <rodrigorib5@gmail.com>
2015-09-25 20:00:50 -0300

    Layout 10

--- application/views/pages/landingpage/home.php
+++ application/views/pages/landingpage/home.php
@@ -13,31 +13,70 @@
 
     <!-- Bootstrap Core CSS -->
     <link href="<?= base_url('public/assets/css/bootstrap.min.css') ?>" rel="stylesheet">
-
     <!-- Custom CSS -->
     <link href="<?= base_url('public/assets/css/atelie.css') ?>" rel="stylesheet">
-
     <!-- Important Owl stylesheet -->
     <link href="<?= base_url('public/assets/css/owl.carousel.css') ?>"  rel="stylesheet">
-
     <!-- Default Theme -->
     <link href="<?= base_url('public/assets/css/owl.theme.css') ?>"  rel="stylesheet">
-
     <!-- Custom Fonts -->
     <link href="<?= base_url('public/assets/font-awesome/css/font-awesome.min.css') ?>" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Dancing+Script' rel='stylesheet' type='text/css'>
     <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <!-- jQuery -->
+    <link rel="stylesheet" href="<?= base_url('public/assets/css/supersized.css') ?>" type="text/css" media="screen" />
+    <script type="text/javascript" src="<?= base_url('public/assets/js/jquery.easing.min.js') ?>"></script>
+    <script src="<?= base_url('public/assets/js/jquery.js') ?>"></script>
+    <script src="<?= base_url('public/assets/js/supersized.3.2.7.min.js') ?>"></script>
 
 </head>
+<script>
+    
+    jQuery(function($){
+                $.supersized({
+                        // Functionality
+                        slideshow      : 1, // Slideshow on/off
+                        autoplay        : 1, // Slideshow starts playing automatically
+                        start_slide     : 1, // Start slide (0 is random)
+                        stop_loop      : 0, // Pauses slideshow on last slide
+                        random         : 0, // Randomize slide order (Ignores start slide)
+                        slide_interval : 3000,// Length between transitions
+                        transition       : 1, // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+                        transition_speed: 1000, // Speed of transition
+                        new_window      : 1, // Image links open in new window/tab
+                        pause_hover     : 0, // Pause slideshow on hover
+                        keyboard_nav    : 1, // Keyboard navigation on/off
+                        performance     : 1, // 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
+                        image_protect   : 1, // Disables image dragging and right click with Javascript
 
+                        // Size & Position
+                        min_width       : 0, // Min width allowed (in pixels)
+                        min_height      : 0, // Min height allowed (in pixels)
+                        vertical_center : 1, // Vertically center background
+                        horizontal_center:1, // Horizontally center background
+                        fit_always      : 0, // Image will never exceed browser width or height (Ignores min. dimensions)
+                        fit_portrait    : 1, // Portrait images will not exceed browser height
+                        fit_landscape   : 0, // Landscape images will not exceed browser width
+
+                        // Components
+                        slide_links     : 'blank',  // Individual links for each slide (Options: false, 'num', 'name', 'blank')
+                        thumb_links     : 1, // Individual thumb links for each slide
+                        thumbnail_navigation : 0, // Thumbnail navigation
+                        slides          :[ // Slideshow Images
+                                        {image : 'public/images/produtos/ingredientes.jpg', title : 'Enroladinhos'},
+                                        {image : 'public/images/produtos/miniBaguetes.jpg', title : 'Brigadeiro'},
+                                        {image : 'public/images/produtos/cupcakes.jpg', title : 'cupcakes'},
+                                        {image : 'public/images/produtos/cupcakesBranco.jpg', title : 'brigadeiro'},
+                                    ],
+
+                        // Theme Options
+                        progress_bar    : 1, // Timer for each slide
+                        mouse_scrub     : 0
+
+                });
+            });
+
+</script>
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
 
     <!-- Navigation -->
@@ -54,7 +93,7 @@
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
-                <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav pull-right">
                     <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
                     <li class="hidden">
                         <a href="#page-top"></a>
@@ -159,9 +198,6 @@
                     <li>
                         <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
                     </li>
-                    <li>
-                        <a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
-                    </li>
                 </ul>
             </div>
         </div>
@@ -177,34 +213,14 @@
         </div>
     </footer>
 
-    <!-- jQuery -->
-    <script src="<?= base_url('public/assets/js/jquery.js') ?>"></script>
-
     <!-- Include js plugin -->
     <script src="<?= base_url('public/assets/js/owl.carousel.min.js') ?>"></script>
-
     <!-- Bootstrap Core JavaScript -->
     <script src="<?= base_url('public/assets/js/bootstrap.min.js') ?>"></script>
-
     <!-- Plugin JavaScript -->
     <script src="<?= base_url('public/assets/js/jquery.easing.min.js') ?>"></script>
-
     <!-- Custom Theme JavaScript -->
     <script src="<?= base_url('public/assets/js/atelie.js') ?>"></script>
 
 </body>
-<script>
-$(document).ready(function() {
-
-  $("#owl-example").owlCarousel({
-    paginationSpeed: 600,
-    pagination: false,
-    navigation: false,
-    singleItem: true,
-    slideSpeed: 600,
-    autoPlay: 3000
-  });
-
-});
-</script>
 </html>
